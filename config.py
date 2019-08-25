fca1c7a83238a6b295f97e5a2eacf59958aceffc
AndrewTrohin <AndrewTrohin@yandex.ru>
2019-08-25 18:41:19 +0300

    Изменение пользователя БД

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'super-secret-key'
     SQLALCHEMY_ECHO = False
     SQLALCHEMY_TRACK_MODIFICATIONS = True
-    SQLALCHEMY_DATABASE_URI = "postgresql://{DB_USER}:{DB_PASS}@{DB_ADDR}/{DB_NAME}".format(DB_USER="postgres",
-                                                                                            DB_PASS="postgres",
+    SQLALCHEMY_DATABASE_URI = "postgresql://{DB_USER}:{DB_PASS}@{DB_ADDR}/{DB_NAME}".format(DB_USER="shop_r",
+                                                                                            DB_PASS="shop_r",
                                                                                             DB_ADDR="127.0.0.1",
                                                                                             DB_NAME="shop")
